f78b7b30b168378f0e41cd62cfe54c7eea7b55ef
SD <sourish.d@gmail.com>
2022-07-01 13:26:29 -0500

    removing access control headers

--- server.js
+++ server.js
@@ -23,20 +23,14 @@ if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
 
-//CORS
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   })
 const cors = require('cors')
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+//   });
 
 //routes
 app.use('/', routes)
