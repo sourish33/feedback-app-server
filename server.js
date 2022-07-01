8d99413800f853279569577ee580ef86d83dad7d
SD <sourish.d@gmail.com>
2022-07-01 00:20:41 -0500

    also enabling  cors together with code

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ if (process.env.NODE_ENV === 'development'){
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   })
-// const cors = require('cors')
-// app.use(cors())
+const cors = require('cors')
+app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
